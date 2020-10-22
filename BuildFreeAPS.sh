b0dd916dfa47bfa03459b3b4dae1e78b0f4e6cdc
jonfawcett <38429455+jonfawcett@users.noreply.github.com>
2020-10-22 15:43:35 -0400

    Add files via upload

---
+++ BuildFreeAPS.sh
@@ -0,0 +1,14 @@
+Echo     Set environment variables
+LOOP_BUILD=$(date +'%y%m%d-%H%M')
+LOOP_DIR=~/Downloads/BuildLoop/FreeAPS-$LOOP_BUILD
+Echo make directories using format year month date hour minute so it can be easily sorted
+mkdir ~/Downloads/BuildLoop/
+mkdir $LOOP_DIR
+cd $LOOP_DIR
+pwd
+Echo download software from github
+git clone --branch=freeaps --recurse-submodules https://github.com/ivalkou/LoopWorkspace
+cd LoopWorkspace
+Echo Open xcode
+xed .
+exit
