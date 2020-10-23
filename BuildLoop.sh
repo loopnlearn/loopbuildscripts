59204bde2a74167ddb2532ab4260a2479fa9d383
jonfawcett <38429455+jonfawcett@users.noreply.github.com>
2020-10-22 20:16:23 -0400

    Update BuildLoop.sh
    Add prompt for Loop version

--- BuildLoop.sh
+++ BuildLoop.sh
@@ -1,13 +1,41 @@
+clear
+echo -e "\nPlease select which version of Loop you would like to download and build"
+options=("FreeAPS" "Loop Master Branch" "Loop Auto Bolus Branch")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "FreeAPS")
+            FOLDERNAME=FreeAPS
+            BRANCH=freeaps
+            REPO=https://github.com/ivalkou/LoopWorkspace
+            break
+            ;;
+        "Loop Master Branch")
+            FOLDERNAME=Loop-Master
+            BRANCH=master
+            REPO=https://github.com/LoopKit/LoopWorkspace
+            break
+            ;;
+        "Loop Auto Bolus Branch")
+            FOLDERNAME=Loop-AutoBolus
+            BRANCH=automatic-bolus
+            REPO=https://github.com/LoopKit/LoopWorkspace
+            break
+            ;;
+        *) 
+    esac
+done
+
 Echo     Set environment variables
 LOOP_BUILD=$(date +'%y%m%d-%H%M')
-LOOP_DIR=~/Downloads/BuildLoop/Loop-$LOOP_BUILD
+LOOP_DIR=~/Downloads/BuildLoop/$FOLDERNAME-$LOOP_BUILD
 Echo make directories using format year month date hour minute so it can be easily sorted
 mkdir ~/Downloads/BuildLoop/
 mkdir $LOOP_DIR
 cd $LOOP_DIR
 pwd
 Echo download software from github
-git clone --branch=master --recurse-submodules https://github.com/LoopKit/LoopWorkspace
+git clone --branch=$BRANCH --recurse-submodules $REPO
 cd LoopWorkspace
 Echo Open xcode
 xed .
