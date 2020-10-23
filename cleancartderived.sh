743683c1e4fb4e035101283d76d396f8b4010267
markjoldham <52680740+markjoldham@users.noreply.github.com>
2020-10-22 17:23:35 -0700

    Delete cleancartderived.sh

--- cleancartderived.sh
+++
@@ -1,5 +0,0 @@
-echo Cleaning carthage files
-rm -rf ~/Library/Caches/org.carthage.CarthageKit
-echo Cleaning Derived Data files
-rm -rf ~/Library/Developer/Xcode/DerivedData
-echo Done cleaning.    If xCode is open, Quit it and open it again.
