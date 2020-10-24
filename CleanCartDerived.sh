eddb5e1ea3441f2c3ee2a03d939c57fbf6fa4a87
markjoldham <52680740+markjoldham@users.noreply.github.com>
2020-10-24 00:09:42 -0700

    Update CleanCartDerived.sh

--- CleanCartDerived.sh
+++ CleanCartDerived.sh
@@ -1,3 +1,7 @@
+echo      Launch this script by pasting this command into a black terminal window.  
+echo .
+echo .     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/loopnlearn/LoopBuildScripts/main/CleanCartDerived.sh)"
+echo .
 echo Cleaning carthage files
 rm -rf ~/Library/Caches/org.carthage.CarthageKit
 echo Cleaning Derived Data files
