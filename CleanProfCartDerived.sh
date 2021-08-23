0a0dfa4452b50e32ef7c5ae40b784012acd899f3
jonfawcett <38429455+jonfawcett@users.noreply.github.com>
2021-08-23 10:55:01 -0400

    Update CleanProfCartDerived.sh
    Remove carthage

--- CleanProfCartDerived.sh
+++ CleanProfCartDerived.sh
@@ -4,8 +4,6 @@ clear
 echo -e "\n\n🕒 Please be patient. On older computers and virtual machines, this may take 5-10 minutes or longer to run.\n"
 echo -e "\n\n✅ Cleaning Profiles.\n"
 rm -rf ~/Library/MobileDevice/Provisioning\ Profiles
-echo -e "\n\n✅ Cleaning Carthage Files.\n"
-rm -rf ~/Library/Caches/org.carthage.CarthageKit
 echo -e "✅ Cleaning Derived Data files.\n"
 rm -rf ~/Library/Developer/Xcode/DerivedData
 echo -e "✅ Done Cleaning.\n\n⚠️  If Xcode is open, Quit it and open it again.\n"
