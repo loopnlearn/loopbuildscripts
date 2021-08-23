bdc3cf5ff6311d3a8b5fb9e62470feecba917b68
jonfawcett <38429455+jonfawcett@users.noreply.github.com>
2021-08-23 10:54:38 -0400

    Update CleanCartDerived.sh
    Remove Carthage

--- CleanCartDerived.sh
+++ CleanCartDerived.sh
@@ -2,8 +2,6 @@
 
 clear
 echo -e "\n\n🕒 Please be patient. On older computers and virtual machines, this may take 5-10 minutes or longer to run.\n"
-echo -e "\n\n✅ Cleaning Carthage Files.\n"
-rm -rf ~/Library/Caches/org.carthage.CarthageKit
 echo -e "✅ Cleaning Derived Data files.\n"
 rm -rf ~/Library/Developer/Xcode/DerivedData
 echo -e "✅ Done Cleaning.\n\n⚠️  If Xcode is open, Quit it and open it again.\n"
