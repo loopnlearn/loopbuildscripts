f90365c734daabbae22f7ac9d7928202d9b82c7c
bjornoleh <bjorn.haugsgjerd@gmail.com>
2022-01-03 12:39:15 +0100

    Download ConfigOverride.xcconfig from loopnlearn
    https://raw.githubusercontent.com/loopnlearn/loopbuildscripts/buildFAX/FAXfiles/ConfigOverride.xcconfig

--- BuildFAX.sh
+++ BuildFAX.sh
@@ -100,7 +100,7 @@ then
     echo -e "\n\n Downloading FAX to your Downloads folder.\n--------------------------------\n"
     git clone --branch=$BRANCH --recurse-submodules $REPO
     cd freeaps
-    curl -O https://raw.githubusercontent.com/bjornoleh/loopbuildscripts/buildFAX/FAXfiles/ConfigOverride.xcconfig
+    curl -O https://raw.githubusercontent.com/loopnlearn/loopbuildscripts/buildFAX/FAXfiles/ConfigOverride.xcconfig
 
     echo -e "--------------------------------\n\n🛑 Please check for errors listed above before proceeding. If there are no errors listed, code has successfully downloaded.\n"
     echo -e "Type 1 and hit enter to open Xcode. You may close the terminal after Xcode opens\n\n"
