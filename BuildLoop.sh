bf1eb5941d14144ec83185a187eaf66b2768cdb8
marionbarker <marionbarker@earthlink.net>
2022-04-03 21:58:06 -0700

    Update script * add FRESH_CLONE flag to enable testing without cloning every time * clean up some language and order of operation * put in placeholders for when persistent signing can be used

--- BuildLoop.sh
+++ BuildLoop.sh
@@ -9,13 +9,19 @@ LOOP_BUILD=$(date +'%y%m%d-%H%M')
 LOOP_DIR=~/Downloads/BuildLoop/
 SCRIPT_DIR=~/Downloads/BuildLoop/Scripts
 
+# to test the behavior of checking and copying LoopConfigOverride file
+# download this file to your local computer and make it executable
+#    change the value of FRESH_CLONE from 1 to anything else
+# cd to a folder above LoopWorkspace for an existing clone,
+#    execute the script in that folder
+FRESH_CLONE=1
 
 mkdir $LOOP_DIR
 mkdir $SCRIPT_DIR
 
-echo -e "\n\n     ----------\n\n"
+clear
 
-echo -e "${RED}\n\n--------------------------------\n\nImportant\n\nPlease understand that this project:\n-Is highly experimental\n-Is not approved for therapy\n-You take full responsibility for building and running this system and do so at your own risk.\n\nYou may only proceed if you agree..\n\n--------------------------------\n\n${NC}"
+echo -e "${RED}\n\n--------------------------------\n\nImportant\n\nPlease understand that this project:\n- Is highly experimental\n- Is not approved for therapy\n- You take full responsibility for building and running this system and do so at your own risk.\n\nYou may only proceed if you agree..\n\n--------------------------------\n\n${NC}"
 echo -e "Type the number from below and hit enter to proceed."
 options=("Agree" "Disagree")
 select opt in "${options[@]}"
@@ -33,9 +39,9 @@ do
     esac
 done
 
-echo -e "\n\n     ----------\n\n"
+echo -e "\n\n\n\n"
 
-echo -e "\n\n--------------------------------\n\nWelcome to Loop and Learn Scripts.\n\n This script will assist you in preparing your computer as well as downloading and building Loop and Loop Follow. Before you begin, please ensure that you have Xcode installed and your phone is plugged into your computer\n\n--------------------------------\n\n"
+echo -e "\n\n--------------------------------\n\nWelcome to Loop and Learn Scripts.\n\n This script will assist you in preparing your computer,\n   as well as downloading and building Loop and Loop Follow.\n Before you begin, please ensure that you have Xcode installed\n   and your phone is plugged into your computer\n\n--------------------------------\n\n"
 echo -e "Type the number from below and hit enter to proceed."
 options=("Build Loop" "Build Loop Follow" "Utility Scripts" "Cancel")
 select opt in "${options[@]}"
@@ -54,7 +60,7 @@ do
             break
             ;;
         "Cancel")
-            echo -e "\n\n     ----------\n\n"
+            echo -e "\n\n\n\n"
             echo -e "\n${RED}User cancelled!${NC}\n\n⬆️  You can press the up arrow on the keyboard followed by the Enter key to start the script from the beginning.\n\n";
             exit 0
             break
@@ -63,17 +69,17 @@ do
     esac
 done
 
-echo -e "\n\n     ----------\n\n"
+echo -e "\n\n\n\n"
 
 if [ "$WHICH" = "Loop" ]
 then
-
-    echo -e "Please select which version of Loop you would like to download and build.\n\nType the number for the branch and hit enter to select the branch.\nType 4 and hit enter to cancel.\n\n"
-    options=("Master Branch" "FreeAPS" "Cancel")
+    echo -e "\n--------------------------------\n"
+    echo -e "Please select which version of Loop you would like to download and build.\n\nType the number for the selection and hit enter.\nType 3 and hit enter to cancel.\n\n"
+    options=("Loop Master" "FreeAPS" "Cancel")
     select opt in "${options[@]}"
     do
         case $opt in
-            "Master Branch")
+            "Loop Master")
                 FOLDERNAME=Loop-Master
                 REPO=https://github.com/LoopKit/LoopWorkspace
                 BRANCH=master
@@ -90,7 +96,7 @@ then
                 break
                 ;;
             "Cancel")
-                echo -e "\n\n     ----------\n\n"
+                echo -e "\n\n\n\n"
                 echo -e "\n${RED}User cancelled!${NC}\n\n⬆️  You can press the up arrow on the keyboard followed by the Enter key to start the script from the beginning.\n\n";
                 exit 0
                 break
@@ -99,19 +105,25 @@ then
         esac
     done
 
-    echo -e "\n\n     ----------\n\n"
+    echo -e "\n\n\n\n"
     LOOP_DIR=~/Downloads/BuildLoop/$FOLDERNAME-$LOOP_BUILD
-    mkdir $LOOP_DIR
-    cd $LOOP_DIR
+    if [ ${FRESH_CLONE} == 1 ]
+    then
+        mkdir $LOOP_DIR
+        cd $LOOP_DIR
+    fi
+    echo -e "\n\n\n\n"
+    echo -e "\n--------------------------------\n"
+    echo -e " -- Downloading Loop to your Downloads folder --\n"
     pwd
-    echo -e "\n\n     ----------\n\n"
-    echo -e "\n\n Downloading Loop to your Downloads folder.\n--------------------------------\n"
-    git clone --branch=$BRANCH --recurse-submodules $REPO
-    echo -e "--------------------------------\n\n🛑 Please check for errors listed above before proceeding. If there are no errors listed, code has successfully downloaded.\n"
+    if [ ${FRESH_CLONE} == 1 ]
+    then
+        git clone --branch=$BRANCH --recurse-submodules $REPO
+    fi
+    echo -e "\n--------------------------------\n\n🛑 Please check for errors listed above before proceeding.\n   If there are no errors listed, code has successfully downloaded.\n"
     echo -e "Type 1 to continue (ONLY when there are no errors) and then:"
     echo -e "* A helpful graphic will be displayed in your browser"
     echo -e "* Xcode will open with your current download."
-    echo -e "You may close the terminal after Xcode opens\n\n"
 
     options=("Continue" "Cancel")
     select opt in "${options[@]}"
@@ -120,37 +132,47 @@ then
             "Continue")
                 if [ ${LOOPCONFIGOVERRIDE_VALID} == 1 ]
                 then
-                    echo -e "Checking status of persistent sign-in file"
+                    echo -e "\n--------------------------------\n"
+                    # echo -e "Checking status of persistent override file"
                     # determine if the persistent file exists
                     # if so, remind user
                     if [ -e ../LoopConfigOverride.xcconfig ]
                     then
-                        echo -e "You have a persistent LoopConfigOverride.xcconfig located in ~/Downloads/BuildLoop"
+                        echo -e "You have a persistent override file:\n   ~/Downloads/BuildLoop/LoopConfigOverride.xcconfig"
                         echo -e "Check it to confirm your Apple Developer ID is correct"
                         echo -e "If so, your project will be automatically signed"
                     else
                         # make sure the LoopConfigOverride.xcconfig exists in clone
                         if [ -e LoopWorkspace/LoopConfigOverride.xcconfig ]
                         then
-                            echo -e "Copying the LoopConfigOverride file to ~/Downloads/BuildLoop"
+                            echo -e "Copying LoopConfigOverride.xcconfig to ~/Downloads/BuildLoop"
                             echo -e "If you edit this file with your Apple Developer ID"
                             echo -e "then your project will be automatically signed"
                             cp -p LoopWorkspace/LoopConfigOverride.xcconfig ..
                         else
-                            echo -e "This project does not have a persistent sign-in file"
+                            echo -e "This project does not have a persistent override file"
                             echo -e "You must sign the targets the individually"
                         fi
                     fi
+                    echo -e "\n--------------------------------\n"
                 fi
-                open https://www.loopandlearn.org/workspace-build-loop/?fromscript
+                # the helper page displayed depends on validity of persistent override
+                if [ ${LOOPCONFIGOVERRIDE_VALID} == 1 ]
+                then
+                    # change this page to the one (not yet written) for persistent override
+                    open https://www.loopandlearn.org/workspace-build-loop/?fromscript
+                else
+                    open https://www.loopandlearn.org/workspace-build-loop/?fromscript
+                fi
                 cd LoopWorkspace
                 xed .
-                echo -e "Shell Script Completed successfully"
+                echo -e "\nShell Script Completed successfully\n"
+                echo -e "You may close the terminal window now if you want\n\n"
                 exit 0
                 break
                 ;;
             "Cancel")
-            echo -e "\n\n     ----------\n\n"
+            echo -e "\n\n\n\n"
                 echo -e "\n${RED}User cancelled!${NC}\n\n⬆️  You can press the up arrow on the keyboard followed by the Enter key to start the script from the beginning.\n\n";
                 exit 0
                 break
@@ -165,11 +187,11 @@ then
     echo -e "\n\n--------------------------------\n\nDownloading Loop Follow Script\n\n--------------------------------\n\n"
     rm ./BuildLoopFollow.sh
     curl -fsSLo ./BuildLoopFollow.sh https://git.io/JTKEt
-    echo -e "\n\n     ----------\n\n"
+    echo -e "\n\n\n\n"
     source ./BuildLoopFollow.sh
 else
     cd $LOOP_DIR/Scripts
-    echo -e "\n\n     ----------\n\n"
+    echo -e "\n\n\n\n"
     echo -e "\n\n--------------------------------\n\nThese scripts will automate several cleanup options for you.\n\n--------------------------------\n\n➡️  Clean Carthage and Derived Data:\nThis script is used to clean up data from old builds from Xcode. Xcode should be closed when running this script.\n\n➡️  Xcode Cleanup (The Big One):\nThis script is used to clean up “stuff” from Xcode. It is typically used after uninstalling Xcode and before installing a new version of Xcode. It can free up a substantial amount of space. Sometimes you might be directed to use this script to resolve a problem, ‼️  beware that you might be required to fully uninstall and reinstall Xcode after running this script.‼️  Usually, Xcode will recover the simulator and other files it needs without needing to be reinstalled.\n\n"
     echo -e "Type the number from below and hit enter to proceed."
     options=("Clean Derived Data" "Xcode Cleanup (The Big One)" "Clean Profiles and Derived Data" "Cancel")
@@ -180,7 +202,7 @@ else
                 echo -e "\n\n--------------------------------\n\nDownloading Derived Data Script\n\n--------------------------------\n\n"
                 rm ./CleanCartDerived.sh
                 curl -fsSLo ./CleanCartDerived.sh https://raw.githubusercontent.com/loopnlearn/LoopBuildScripts/main/CleanCartDerived.sh
-                echo -e "\n\n     ----------\n\n"
+                echo -e "\n\n\n\n"
                 source ./CleanCartDerived.sh
                 break
                 ;;
@@ -188,7 +210,7 @@ else
                 echo -e "\n\n--------------------------------\n\nDownloading Xcode Cleanup Script\n\n--------------------------------\n\n"
                 rm ./XcodeClean.sh
                 curl -fsSLo ./XcodeClean.sh https://raw.githubusercontent.com/loopnlearn/LoopBuildScripts/main/XcodeClean.sh
-                echo -e "\n\n     ----------\n\n"
+                echo -e "\n\n\n\n"
                 source ./XcodeClean.sh
                 break
                 ;;
@@ -196,12 +218,12 @@ else
                 echo -e "\n\n--------------------------------\n\nDownloading Profiles and Derived Data Script\n\n--------------------------------\n\n"
                 rm ./CleanProfCartDerived.sh
                 curl -fsSLo ./CleanProfCartDerived.sh https://raw.githubusercontent.com/loopnlearn/LoopBuildScripts/main/CleanProfCartDerived.sh
-                echo -e "\n\n     ----------\n\n"
+                echo -e "\n\n\n\n"
                 source ./CleanProfCartDerived.sh
                 break
                 ;;
             "Cancel")
-                echo -e "\n\n     ----------\n\n"
+                echo -e "\n\n\n\n"
                 echo -e "\n${RED}User cancelled!${NC}\n\n⬆️  You can press the up arrow on the keyboard followed by the Enter key to start the script from the beginning.\n\n";
                 exit 0
                 break
