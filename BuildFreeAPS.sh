e678dc2568b464f6e8bb1d9fcf2f2b0098a43596
marionbarker <marionbarker@earthlink.net>
2023-01-21 09:44:10 -0800

    BuildFreeAPS: add stand alone script for FreeAPS

---
+++ BuildFreeAPS.sh
@@ -0,0 +1,143 @@
+#!/bin/bash # script BuildFreeAPS.sh
+
+############################################################
+# this code must be repeated in any build script that uses build_functions.sh
+############################################################
+
+BUILD_DIR=~/Downloads/"BuildLoop"
+SCRIPT_DIR="${BUILD_DIR}/Scripts"
+
+if [ ! -d "${BUILD_DIR}" ]; then
+    mkdir "${BUILD_DIR}"
+fi
+if [ ! -d "${SCRIPT_DIR}" ]; then
+    mkdir "${SCRIPT_DIR}"
+fi
+
+STARTING_DIR="${PWD}"
+
+# change directory to $SCRIPT_DIR before curl calls
+cd "${SCRIPT_DIR}"
+
+# define branch (to make it easier when updating)
+# typically branch is main
+SCRIPT_BRANCH=main
+
+# store a copy of build_functions.sh in script directory
+curl -fsSLo ./build_functions.sh https://raw.githubusercontent.com/loopnlearn/LoopBuildScripts/$SCRIPT_BRANCH/build_functions.sh
+
+# Verify build_functions.sh was downloaded.
+if [ ! -f ./build_functions.sh ]; then
+    echo -e "\n *** Error *** build_functions.sh not downloaded "
+    echo -e "Please attempt to download manually"
+    echo -e "  Copy the following line and paste into terminal\n"
+    echo -e "curl -SLo ~/Downloads/BuildLoop/Scripts/build_functions.sh https://raw.githubusercontent.com/loopnlearn/LoopBuildScripts/main/build_functions.sh"
+    echo -e ""
+    exit
+fi
+
+# This brings in functions from build_functions.sh
+#   When testing update to build_functions.sh, set to 1 for testing only
+DEBUG_FLAG=0
+if [ ${DEBUG_FLAG} == 0 ]; then
+    source ./build_functions.sh
+else
+    source ~/Downloads/ManualClones/lnl/loopbuildscripts/build_functions.sh
+fi
+
+############################################################
+# The rest of this is specific to  BuildFreeAPS.sh
+############################################################
+
+# store a copy of this script.sh in script directory
+curl -fsSLo ./BuildFreeAPS.sh https://raw.githubusercontent.com/loopnlearn/LoopBuildScripts/$SCRIPT_BRANCH/BuildFreeAPS.sh
+
+section_separator
+BRANCH_FREE=freeaps
+LOOPCONFIGOVERRIDE_VALID=1
+echo -e "\n ${RED}${BOLD}You are running the script for FreeAPS${NC}"
+choose_or_cancel
+options=("Continue" "Cancel")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Continue")
+            FORK_NAME=FreeAPS
+            REPO=https://github.com/loopnlearn/LoopWorkspace
+            BRANCH=freeaps
+            break
+            ;;
+        "Cancel")
+            cancel_entry
+            ;;
+        *)
+            invalid_entry
+            ;;
+    esac
+done
+
+LOOP_DIR="${BUILD_DIR}/${FORK_NAME}-${BRANCH}-${DOWNLOAD_DATE}_${FIXED_SHA}"
+if [ ${FRESH_CLONE} == 1 ]; then
+    mkdir "${LOOP_DIR}"
+else
+    LOOP_DIR="${STARTING_DIR}"
+fi
+cd "${LOOP_DIR}"
+section_separator
+if [ ${FRESH_CLONE} == 1 ]; then
+    echo -e " -- Downloading ${FORK_NAME} ${BRANCH} to your Downloads folder --"
+    echo -e "      ${LOOP_DIR}\n"
+    echo -e "Issuing this command:"
+    echo -e "    git clone --branch=${BRANCH} --recurse-submodules ${REPO}"
+    git clone --branch=$BRANCH --recurse-submodules $REPO
+fi
+#
+clone_download_error_check
+options=("Continue" "Cancel")
+select opt in "${options[@]}"
+do
+    section_separator
+    case $opt in
+        "Continue")
+            cd LoopWorkspace
+            echo -e "Continue if no errors reported"
+            choose_or_cancel
+            options=("Continue" "Cancel")
+            select opt in "${options[@]}"
+            do
+                case $opt in
+                    "Continue")
+                        break
+                        ;;
+                    "Cancel")
+                        cancel_entry
+                        ;;
+                      *) # Invalid option
+                         invalid_entry
+                         ;;
+                esac
+            done
+            if [ ${LOOPCONFIGOVERRIDE_VALID} == 1 ]; then
+                check_config_override_existence_offer_to_configure
+            fi
+
+            section_separator
+            echo -e "The following items will open (when you are ready)"
+            echo -e "* Webpage with detailed build steps (LoopDocs)"
+            echo -e "* Xcode ready to prep your current download for build"
+            before_final_return_message
+            return_when_ready
+            open "https://loopkit.github.io/loopdocs/build/step14/#initial-xcode-screens"
+            sleep 2
+            xed .
+            exit_message
+            break
+            ;;
+        "Cancel")
+            cancel_entry
+            ;;
+        *)
+            invalid_entry
+            ;;
+    esac
+done
