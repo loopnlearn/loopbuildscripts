aae8b617decfd57517a2c2850ff7b68d82aae9ad
bjorkert <jonas@bjorkert.se>
2023-04-21 12:45:02 +0200

    initial_greeting

--- custom_config.sh
+++ custom_config.sh
@@ -3,6 +3,11 @@
 # This script sets custom values for BuildLoop it MUST be run using source in order to affect the build scripts.
 # For example: source custom_config.sh&&./BuildLoop.sh
 
+# Clear values before setting them below
+unset SCRIPT_BRANCH
+unset LOCAL_BUILD_FUNCTIONS_PATH
+unset FRESH_CLONE
+
 # SCRIPT_BRANCH is the branch build_functions.sh will be sourced from
 export SCRIPT_BRANCH="xcode-select"
 
