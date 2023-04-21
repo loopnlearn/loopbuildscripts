dd834f3a37e8ae0b9ca25ff3642ac0feb3ccdf7f
marionbarker <marionbarker@earthlink.net>
2023-04-21 15:43:32 -0700

    config scripts: add testing variable, more comments

--- custom_config.sh
+++ custom_config.sh
@@ -8,18 +8,25 @@
 unset SCRIPT_BRANCH
 unset LOCAL_BUILD_FUNCTIONS_PATH
 unset FRESH_CLONE
+unset CLONE_STATUS
 
+# To Test build_functions.sh as they exist on github
 # SCRIPT_BRANCH is the branch build_functions.sh will be sourced from
 #   Uncomment line and replace main with branch you are testing
-# export SCRIPT_BRANCH="main"
+#export SCRIPT_BRANCH="main"
 
+# To Test build_functions.sh as they exist on your local clone
 # LOCAL_BUILD_FUNCTIONS_PATH lets you test build_functions.sh locally
 #   Uncomment line; path should be your local clone of loopbuildscripts
 #export LOCAL_BUILD_FUNCTIONS_PATH="$HOME/projects/loopbuildscripts/build_functions.sh"
-#export LOCAL_BUILD_FUNCTIONS_PATH="$HOME/Downloads/ManualClones/lnl/loopbuildscripts/build_functions.sh"
-
+#export LOCAL_BUILD_FUNCTIONS_PATH="/Users/marion/Downloads/ManualClones/lnl/loopbuildscripts/build_functions.sh"
 
 # FRESH_CLONE (of 0) lets you use an existing LoopWorkspace clone (saves time)
 #   Uncomment the line
 #   The terminal must be one level higher than an existing LoopWorkspace folder
-# export FRESH_CLONE="0"
+#export FRESH_CLONE="0"
+
+# CLONE_STATUS can be set to 0 for success (default) or 1 for error
+#   Uncomment only to trigger an error in the clone check
+#   Pair this with FRESH_CLONE="0"
+#export CLONE_STATUS="1"
