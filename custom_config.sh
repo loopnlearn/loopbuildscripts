4b259516cc6bad4dd6c939fa23935cd597052df4
marionbarker <marionbarker@earthlink.net>
2023-04-21 14:14:07 -0700

    custom_config: update some comments

--- custom_config.sh
+++ custom_config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# This script sets custom values for BuildLoop it MUST be run using source in order to affect the build scripts.
-# For example: source custom_config.sh&&./BuildLoop.sh
+# This script sets custom values for scripts, like BuildLoop.
+#   It MUST be run using source in order to affect the build scripts.
+#   For example: source custom_config.sh && ./BuildLoop.sh
 
 # Clear values before setting them below
 unset SCRIPT_BRANCH
@@ -9,11 +10,16 @@ unset LOCAL_BUILD_FUNCTIONS_PATH
 unset FRESH_CLONE
 
 # SCRIPT_BRANCH is the branch build_functions.sh will be sourced from
-export SCRIPT_BRANCH="xcode-select"
+#   Uncomment line and replace main with branch you are testing
+# export SCRIPT_BRANCH="main"
 
-# Local sourcing of build_functions.sh
+# LOCAL_BUILD_FUNCTIONS_PATH lets you test build_functions.sh locally
+#   Uncomment line; path should be your local clone of loopbuildscripts
 #export LOCAL_BUILD_FUNCTIONS_PATH="$HOME/projects/loopbuildscripts/build_functions.sh"
 #export LOCAL_BUILD_FUNCTIONS_PATH="$HOME/Downloads/ManualClones/lnl/loopbuildscripts/build_functions.sh"
 
-# Use existing loop download instead of cloning a new
-#export FRESH_CLONE="0"
+
+# FRESH_CLONE (of 0) lets you with existing LoopWorkspace clone (saves time)
+#   Uncomment the line
+#   The terminal must be one level higher than an existing LoopWorkspace folder
+# export FRESH_CLONE="0"
