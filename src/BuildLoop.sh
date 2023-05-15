69a84dcd72196f2be72346f2df06d8e2676066d7
marionbarker <marionbarker@earthlink.net>
2023-05-14 21:43:37 -0700

    Menu redesign for BuildLoop

--- src/BuildLoop.sh
+++ src/BuildLoop.sh
@@ -13,12 +13,16 @@ DEV_TEAM_SETTING_NAME="LOOP_DEVELOPMENT_TEAM"
 #!inline delete_old_downloads.sh
 #!inline run_script.sh
 
+function placeholder() {
+    echo -e "The feature is not ready"
+    echo -e "This is a placeholder"
+}
 
 ############################################################
 # The rest of this is specific to the particular script
 ############################################################
 
-initial_greeting "https://loopdocs.org"
+initial_greeting
 
 
 ############################################################
@@ -26,16 +30,29 @@ initial_greeting "https://loopdocs.org"
 ############################################################
 
 section_separator
-echo -e "${BOLD}Welcome to the Loop and Learn\n  Build-Select Script\n${NC}"
+echo -e "${RED}${BOLD}Welcome to the Loop and Learn\n  Build-Select Script\n${NC}"
 echo -e "This script will help you to:"
-echo -e "  1 Download and build Loop"
-echo -e "  2 Download and build LoopFollow"
-echo -e "  3 Select a Utility Script to assist when updating your computer"
-echo -e "     or Customizing Loop"
-echo -e "\nRun the script again to choose a different task"
+echo -e "  1 Download and Build Loop"
+echo -e "  2 Download and Build Related Apps"
+echo -e "  3 Run Maintenance Utilities"
+echo -e "  4 Run Customization Utilities"
+echo -e ""
+echo -e "Run the script again to choose a different option"
+echo -e "You need Xcode and Xcode command line tools installed"
+section_divider
 
-options=("Build Loop" "Build LoopFollow" "Utility Scripts" "Cancel")
-actions=("WHICH=Loop" "WHICH=LoopFollow" "WHICH=UtilityScripts" "cancel_entry")
+options=(\
+    "Build Loop" \
+    "Build Related Apps" \
+    "Maintenance Utilities" \
+    "Customization Utilities" \
+    "Cancel")
+actions=(\
+    "WHICH=Loop" \
+    "WHICH=OtherApps" \
+    "WHICH=UtilityScripts" \
+    "WHICH=CustomizationScripts" \
+    "cancel_entry")
 menu_select "${options[@]}" "${actions[@]}"
 
 if [ "$WHICH" = "Loop" ]; then
@@ -49,15 +66,17 @@ if [ "$WHICH" = "Loop" ]; then
         }
         
         section_separator
-        echo -e "Before you continue, please ensure"
-        echo -e "  you have Xcode and Xcode command line tools installed\n"
-        echo -e "Please select which version of Loop to download and build."
-        echo -e "\n  Loop:"
+        echo -e "${RED}${BOLD}You should be familiar with the documenation found at:${NC}"
+        echo -e "   https://loopdocs.org"
+        echo -e ""
+        echo -e "Select which version of Loop to download and build."
+        echo -e "   Loop:"
         echo -e "      This is always the current released version"
         echo -e "      More info at https://github.com/LoopKit/Loop/releases"
-        echo -e "\n  Loop with Patches:"
+        echo -e "   Loop with Patches:"
         echo -e "      adds 2 CGM options, CustomTypeOne LoopPatches, new Logo"
         echo -e "      More info at https://www.loopandlearn.org/main-lnl-patches"
+        section_divider
 
         options=("Loop" "Loop with Patches" "Cancel")
         actions=("choose_loop" "choose_loop_with_patches" "cancel_entry")
@@ -83,52 +102,83 @@ if [ "$WHICH" = "Loop" ]; then
     xed .
     exit_message
 
-elif [ "$WHICH" = "LoopFollow" ]
-then
-    run_script "BuildLoopFollow.sh" $CUSTOM_BRANCH
-else
-    echo -e "\n\n\n\n"
-    echo -e "\n--------------------------------\n"
-    echo -e "${BOLD}These utility scripts automate several cleanup actions${NC}"
-    echo -e "\n--------------------------------\n"
-    echo -e "1 ➡️  Clean Derived Data:\n"
-    echo -e "    This script is used to clean up data from old builds."
-    echo -e "    In other words, it frees up space on your disk."
-    echo -e "    Xcode should be closed when running this script.\n"
-    echo -e "2 ➡️  Xcode Cleanup (The Big One):\n"
-    echo -e "    This script clears even more disk space filled up by using Xcode."
-    echo -e "    It is typically used after uninstalling Xcode"
-    echo -e "      and before installing a new version of Xcode."
-    echo -e "    It can free up a substantial amount of disk space."
-    echo -e "\n    You might be directed to use this script to resolve a problem."
-    echo -e "\n${RED}${BOLD}    Beware that you might be required to fully uninstall"
-    echo -e "      and reinstall Xcode if you run this script with Xcode installed.\n${NC}"
-    echo -e "    Always a good idea to reboot your computer after Xcode Cleanup.\n"
-    echo -e "3 ➡️  Clean Profiles:\n"
-    echo -e "    Incorporated in the BuildLoop section"
-    echo -e "    No longer needed as a stand-alone step.\n"
-    echo -e "4 ➡️  Apply Customizations to Loop:\n"
-    echo -e "    The customizations are documented here:"
-    echo -e "    https://www.loopandlearn.org/custom-code/#custom-list"
-    echo -e "5 ➡️  Delete old downloads :\n"
-    echo -e "    TODO: Describe this feature..."
-    echo -e "\n--------------------------------\n"
-    echo -e "${RED}${BOLD}You may need to scroll up in the terminal to see details about options${NC}"
+elif [ "$WHICH" = "OtherApps" ]; then
+
+    section_separator
+    echo -e "Select the app you want to build"
+    echo -e "  Each selection will indicate documentation links"
+    echo -e "  Please read the documentation before using the app"
+    echo -e ""
+    options=(\
+        "Build Loop Follow" \
+        "Build LoopCaregiver" \
+        "Build xDrip4iOS" \
+        "Cancel")
+    actions=(\
+        "WHICH=LoopFollow" \
+        "WHICH=LoopCaregiver" \
+        "WHICH=xDrip4iOS" \
+        "cancel_entry")
+    menu_select "${options[@]}" "${actions[@]}"
+    if [ "$WHICH" = "LoopFollow" ]; then
+        run_script "BuildLoopFollow.sh" $CUSTOM_BRANCH
+    elif [ "$WHICH" = "LoopCaregiver" ]; then
+        run_script "BuildLoopCaregiver.sh" $CUSTOM_BRANCH
+    elif [ "$WHICH" = "xDrip4iOS" ]; then
+        run_script "BuildxDrip4iOS.sh" $CUSTOM_BRANCH
+    fi
+
+elif [ "$WHICH" = "UtilityScripts" ]; then
+
+    section_separator
+    echo -e "${RED}${BOLD}These utility scripts automate several cleanup actions${NC}"
+    echo -e ""
+    echo -e " 1. Clean Derived Data:"
+    echo -e "     Free space on your disk from old Xcode builds."
+    echo -e "     You should quit Xcode before running this script."
+    echo -e " 2. Xcode Cleanup (The Big One):"
+    echo -e "     Clears more disk space filled up by using Xcode."
+    echo -e "     * Use after uninstalling Xcode prior to new installation"
+    echo -e "     * It can free up a substantial amount of disk space"
+    echo -e " 3. Delete Old Downloads:"
+    echo -e "     Not ready for release - placeholder..."
+    section_divider
 
     options=(
         "Clean Derived Data"
-        "Xcode Cleanup (The Big One)"
-        "Clean Profiles"
-        "Apply Customizations to Loop"
-        "Delete old downloads"
+        "Xcode Cleanup"
+        "Delete Old Downloads"
         "Cancel"
     )
     actions=(
         "run_script 'CleanDerived.sh'"
         "run_script 'XcodeClean.sh'"
-        "run_script 'CleanProfiles.sh'"
+        "placeholder"
+        "cancel_entry"
+    )
+    menu_select "${options[@]}" "${actions[@]}"
+
+else
+    section_separator
+    echo -e "${RED}${BOLD}Selectable Customizations for:${NC}"
+    echo -e "    Released code: Loop or Loop with Patches"
+    echo -e "    Might work for development branches of Loop"
+    echo -e ""
+    echo -e "Reports status for each customization:"
+    echo -e "    can be or has been applied or is not applicable"
+    echo -e ""
+    echo -e "Automatically finds most recent Loop download unless"
+    echo -e "    terminal is already at the LoopWorkspace folder level"
+    section_divider
+
+    options=(
+        "Loop Customizations"
+        "Placeholder"
+        "Cancel"
+    )
+    actions=(
         "run_script 'CustomizationSelect.sh'"
-        "delete_old_downloads"
+        "placeholder"
         "cancel_entry"
     )
     menu_select "${options[@]}" "${actions[@]}"
