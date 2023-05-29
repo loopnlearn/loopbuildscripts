12aaa7da49e93dcf3c153bf49adbd54de8753c15
Jonas Björkert <jonas@bjorkert.se>
2023-05-29 19:54:21 +0200

    lnl patches

--- CustomizationSelect.sh
+++ CustomizationSelect.sh
@@ -473,9 +473,18 @@ function patch_menu {
 add_customization "Profiles" "profile"
 add_customization "Enhanced AutoBolus" "ab_ramp"
 add_customization "Enhanced AutoBolus with Modified CustomTypeOne LoopPatches" "ab_ramp_cto"
-add_customization "CAGE update for Omnipod" "cage"
-add_customization "Dexcom Upload readings" "dexcom_upload_readings"
 add_customization "CustomTypeOne LoopPatches (original)" "customtypeone_looppatches"
+
+add_customization "Increase Future Carbs Limit to 4 hours" "future_carbs_4h"
+add_customization "Libre Users: Limit Loop to <5 minutes" "limit_loop_cycle_time"
+add_customization "Modify Carb Warning & Limit: Low Carb to 49 & 99" "low_carb_limit"
+add_customization "Modify Carb Warning & Limit: High Carb to 201 & 300" "high_carb_limit"
+add_customization "Disable Authentication Requirement" "no_auth"
+add_customization "Override Insulin Needs Picker (50% to 200%, steps of 5%)" "override_sens"
+add_customization "CAGE: Upload Pod Start to Nightscout" "omnipod_cage"
+add_customization "SAGE: Upload Dexcom Sensor Start to Nightscout" "dexcom_sage"
+add_customization "Change Default to Upload Dexcom Readings" "dexcom_upload_readings"
+add_customization "Modify Logo with LnL icon" "lnl_icon"
 
 function customization_info {
     echo "The Prepared Customizations are documented on the Loop and Learn web site"
