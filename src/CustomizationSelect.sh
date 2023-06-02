1701a216ba489216563a6b4715fadf4b9443a67a
marionbarker <marionbarker@earthlink.net>
2023-06-02 07:46:47 -0700

    Reorder so PR1988 comes after CTO (original)

--- src/CustomizationSelect.sh
+++ src/CustomizationSelect.sh
@@ -43,22 +43,26 @@ function message_for_ab_ramp() {
     message_incompatible
 }
 
-# index 0 to 4
+# list patches in this order with args:
+#   User facing information for option
+#   Folder name in the patch repo
+#   (Optional) message function shown prior to option
+
 add_customization "CAGE: Upload Pod Start to Nightscout" "omnipod_cage"
 add_customization "SAGE: Upload Dexcom Sensor Start to Nightscout" "dexcom_sage"
 add_customization "Change Default to Upload Dexcom Readings" "dexcom_upload_readings"
 add_customization "Increase Future Carbs Limit to 4 hours" "future_carbs_4h"
 add_customization "Modify Carb Warning & Limit: Low Carb to 49 & 99" "low_carb_limit"
-# index 5 to 9
+
 add_customization "Modify Carb Warning & Limit: High Carb to 201 & 300" "high_carb_limit"
 add_customization "Disable Authentication Requirement" "no_auth"
 add_customization "Override Insulin Needs Picker (50% to 200%, steps of 5%)" "override_sens"
 add_customization "Libre Users: Limit Loop to 5 minute update" "limit_loop_cycle_time"
 add_customization "Modify Logo with LnL icon" "lnl_icon"
-# index 10 to 13
+
 add_customization "CustomTypeOne LoopPatches (original)" "customtypeone_looppatches" "message_incompatible"
-add_customization "Profiles (PR 2002)" "profile" "message_for_profiles"
 add_customization "Enhanced AutoBolus (PR 1988)" "ab_ramp" "message_for_ab_ramp"
 add_customization "Enhanced AutoBolus (PR 1988) with Modified CustomTypeOne LoopPatches" "ab_ramp_cto"
+add_customization "Profiles (PR 2002)" "profile" "message_for_profiles"
 
 patch_menu
