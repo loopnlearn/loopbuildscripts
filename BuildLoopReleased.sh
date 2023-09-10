7c893de93e6f16a05a3121731be624e1ac513954
marionbarker <marionbarker@earthlink.net>
2023-09-10 10:17:52 -0700

    Remove main_lnl_patches as an option

--- BuildLoopReleased.sh
+++ BuildLoopReleased.sh
@@ -755,33 +755,25 @@ open_source_warning
 
 
 URL_THIS_SCRIPT="https://github.com/LoopKit/LoopWorkspace.git"
-URL_FOR_LNL="https://github.com/loopnlearn/LoopWorkspace.git"
 
 if [ -z "$CUSTOM_BRANCH" ]; then
     function choose_loop() {
         branch_select ${URL_THIS_SCRIPT} main Loop
-    }
-
-    function choose_loop_with_patches() {
-        branch_select ${URL_FOR_LNL} main_lnl_patches Loop_lnl_patches
     }
     
     section_separator
     echo -e "${INFO_FONT}You should be familiar with the documenation found at:${NC}"
     echo -e "   https://loopdocs.org"
     echo -e ""
-    echo -e "Select which version of Loop to download and build."
+    echo -e "You will be building"
     echo -e "   Loop:"
     echo -e "      This is always the current released version"
     echo -e "      More info at https://github.com/LoopKit/Loop/releases"
-    echo -e "   Loop with Patches:"
-    echo -e "      Adds 2 CGM options, CustomTypeOne LoopPatches, new Logo"
-    echo -e "      More info at https://www.loopandlearn.org/main-lnl-patches"
     echo -e "You need Xcode and Xcode command line tools installed"
     section_divider
 
-    options=("Loop" "Loop with Patches" "$(exit_or_return_menu)")
-    actions=("choose_loop" "choose_loop_with_patches" "exit_script")
+    options=("Loop" "$(exit_or_return_menu)")
+    actions=("choose_loop" "exit_script")
     menu_select "${options[@]}" "${actions[@]}"
 else
     section_separator
