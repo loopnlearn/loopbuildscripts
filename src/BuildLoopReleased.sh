f903c8ea3019a3cb5061b6d8acf4317589e7bdb4
marionbarker <marionbarker@earthlink.net>
2025-09-03 11:53:16 -0700

    clean up comments

--- src/BuildLoopReleased.sh
+++ src/BuildLoopReleased.sh
@@ -2,10 +2,11 @@
 
 script_name="BuildLoopReleased.sh"
 
-# remove and add special font
+# add special font
 NC='\033[0m'
 ERROR_FONT='\033[1;31m'
 
+# redirect message
 echo -e "${ERROR_FONT}The ${script_name} script has been superceded:${NC}"
 echo "  Use the BuildLoop script instead:"
 echo "/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/loopandlearn/lnl-scripts/main/BuildLoop.sh)\""
