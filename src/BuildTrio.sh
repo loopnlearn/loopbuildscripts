3cb99de5b84a34370e1c59a21d72e79080454b66
marionbarker <marionbarker@earthlink.net>
2025-11-19 11:14:52 -0800

    update BuildTrio to only offer dev branch

--- src/BuildTrio.sh
+++ src/BuildTrio.sh
@@ -74,18 +74,23 @@ if [ -z "$CUSTOM_BRANCH" ]; then
         echo -e "       Website       : ${URL_FOR_WEBSITE}"
         echo -e "       Documentation : ${URL_FOR_DOCS}"
         echo -e ""
-        echo -e "  ${INFO_FONT}Option 1: Trio main is recommended${NC}"
+        echo -e "  ${INFO_FONT}Trio dev is recommended${NC}"
+        echo -e "  ${INFO_FONT}* Check the settings screen frequently${NC}"
+        echo -e "  ${INFO_FONT}* Download/rebuild when latest dev is at a higher version${NC}"
+        echo -e ""
+        echo -e "  If you insist on building Trio main, you can no longer use this script."
+        echo -e "  Ask a mentor for help."
         echo -e ""
         echo -e "Before you continue, please ensure"
         echo -e "  you have Xcode and Xcode command line tools installed\n"
 
         options=(\
-            "${app_name} main" \
+            #"${app_name} main" \
             "${app_name} dev" \
             # "${app_name} ${special_branch_name}" \
             "$(exit_or_return_menu)")
         actions=(\
-            "select_main" \
+            #"select_main" \
             "select_dev" \
             # "select_special_branch" \
             "exit_script")
